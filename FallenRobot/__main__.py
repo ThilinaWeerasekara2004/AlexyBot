4644c0f300fc2fdfdce18b266b504018712656ae
Tashith Manuka <131139722+TashithManuka006@users.noreply.github.com>
2023-08-26 21:09:22 +0530

    Update __main__.py
    Signed-off-by: Tashith Manuka <131139722+TashithManuka006@users.noreply.github.com>

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -102,16 +102,6 @@ buttons = [
     ],
 ]
 
-@app.on_callback_query(filters.regex("gib_source") & ~BANNED_USERS)
-@languageCB
-async def gib_repo(client, CallbackQuery, _):
-    await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://te.legra.ph/file/f40cc84c13b2954c2ba40.mp4"),
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
-        ),
-    )
-
 HELP_STRINGS = f"""
 *» {BOT_NAME} ᴇxᴄʟᴜsɪᴠᴇ ꜰᴇᴀᴛᴜʀᴇs*
 
