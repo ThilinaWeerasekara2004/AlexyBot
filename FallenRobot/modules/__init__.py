0fa187e6e3cbeb7e2ec0173d92cac101dc008c67
ThilinaWeerasekara2004 <145570119+ThilinaWeerasekara2004@users.noreply.github.com>
2025-04-09 16:18:23 +0530

    Update __init__.py
    Signed-off-by: ThilinaWeerasekara2004 <145570119+ThilinaWeerasekara2004@users.noreply.github.com>

--- FallenRobot/modules/__init__.py
+++ FallenRobot/modules/__init__.py
@@ -1,11 +1,14 @@
 from FallenRobot import LOAD, LOGGER, NO_LOAD
+import glob
+from os.path import basename, dirname, isfile
 
 
 def __list_all_modules():
-    import glob
-    from os.path import basename, dirname, isfile
-
-    # This generates a list of modules in this folder for the * in __main__ to work.
+    """
+    Function to list all available modules and determine which ones to load
+    based on the LOAD and NO_LOAD conditions.
+    """
+    # Generate a list of module paths in the current directory.
     mod_paths = glob.glob(dirname(__file__) + "/*.py")
     all_modules = [
         basename(f)[:-3]
@@ -13,29 +16,26 @@ def __list_all_modules():
         if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ]
 
-    if LOAD or NO_LOAD:
+    # If there's a LOAD condition, validate the modules specified.
+    if LOAD:
         to_load = LOAD
-        if to_load:
-            if not all(
-                any(mod == module_name for module_name in all_modules)
-                for mod in to_load
-            ):
-                LOGGER.error("Invalid loadorder names, Quitting...")
-                quit(1)
+        invalid_modules = [mod for mod in to_load if mod not in all_modules]
+        if invalid_modules:
+            LOGGER.error("Invalid loadorder names: %s. Quitting...", ", ".join(invalid_modules))
+            quit(1)
 
-            all_modules = sorted(set(all_modules) - set(to_load))
-            to_load = list(all_modules) + to_load
+        # Remove already loaded modules and prioritize loading the modules specified in LOAD.
+        all_modules = sorted(set(all_modules) - set(to_load))
+        to_load = list(all_modules) + to_load
+    else:
+        to_load = all_modules
 
-        else:
-            to_load = all_modules
+    # If NO_LOAD is set, exclude the modules specified from the loading list.
+    if NO_LOAD:
+        LOGGER.info("Not loading: %s", ", ".join(NO_LOAD))
+        to_load = [item for item in to_load if item not in NO_LOAD]
 
-        if NO_LOAD:
-            LOGGER.info("Not loading: {}".format(NO_LOAD))
-            return [item for item in to_load if item not in NO_LOAD]
-
-        return to_load
-
-    return all_modules
+    return to_load
 
 
 ALL_MODULES = __list_all_modules()
