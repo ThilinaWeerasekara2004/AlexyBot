bf7a70d7f4fb061397da306454cbc1f8fe753f88
ThilinaWeerasekara2004 <145570119+ThilinaWeerasekara2004@users.noreply.github.com>
2025-04-10 14:23:55 +0530

    Update config.py
    Signed-off-by: ThilinaWeerasekara2004 <145570119+ThilinaWeerasekara2004@users.noreply.github.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -7,7 +7,7 @@ class Config(object):
 
     CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = ""
 
     EVENT_LOGS = ()  # Event logs channel to note down important bot level events
 
